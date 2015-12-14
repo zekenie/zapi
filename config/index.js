77d6075b18328484b65508bdfafd46b20ffbb42a
Zeke Nierenberg <hampzan09@gmail.com>
2015-12-14 09:47:01 -0600

    passphrase

--- config/index.js
+++ config/index.js
@@ -6,5 +6,6 @@ module.exports = {
   https: {
     key: fs.readFileSync(process.env.PRIVATE_KEY || `${process.cwd()}/devKeys/key.pem`, 'utf8'),
     cert: fs.readFileSync(process.env.CERT || `${process.cwd()}/devKeys/cert.pem`, 'utf8'),
+    passphrase: process.env.SSL_PASSWORD
   }
 };
