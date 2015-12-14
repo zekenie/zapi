8077227e3685574c4c802a5badd3ed5653c85ffc
Zeke Nierenberg <hampzan09@gmail.com>
2015-12-14 12:01:39 -0600

    silly error

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 module.exports = {
   mongoURI: 'mongodb://localhost/zeke-data',
-  port: 8999 || process.env.PORT,
+  port: process.env.PORT || 8999,
   https: {
     key: fs.readFileSync(process.env.PRIVATE_KEY || `${process.cwd()}/devKeys/key.pem`, 'utf8'),
     cert: fs.readFileSync(process.env.CERT || `${process.cwd()}/devKeys/cert.pem`, 'utf8'),
