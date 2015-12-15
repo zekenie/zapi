d2eb8fee0192eb2088036550cf8ec931c736cde1
Zeke Nierenberg <hampzan09@gmail.com>
2015-12-14 18:41:54 -0600

    logging

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const https = require('https');
 const app = express();
+const morgan = require('morgan');
 const config = require('./config');
 
 https.createServer(config.https, app).listen(config.port);
 
 require('./models');
+app.use(morgan('dev'));
 app.use(require('./api'));
