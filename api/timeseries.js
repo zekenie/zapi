2bcf4c038e1e0037e31ce40630d5055888a5874e
Zeke Nierenberg <hampzan09@gmail.com>
2015-12-30 09:12:58 -0600

    reading timeseries

--- api/timeseries.js
+++ api/timeseries.js
@@ -21,4 +21,18 @@ router.post('/bulk', (req, res, next) => {
     .catch(next);
 });
 
+router.get('/:dataset', (req, res, next) => {
+  const query = {};
+  if(req.query.start) {
+    query.__date = { $gte: Date(req.query.start) };
+  }
+  if(req.query.end) {
+    query.__date = query.__date || {};
+    query.__date.$lte = Date(req.query.end);
+  }
+  mongoose.model(req.params.dataset).find(query)
+    .then( values => res.json(values) )
+    .catch(next);
+});
+
 module.exports = router;
