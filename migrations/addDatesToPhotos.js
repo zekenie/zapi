d3567b6c43e239a426a71531fe39f9f613998294
Zeke Nierenberg <hampzan09@gmail.com>
2016-01-02 12:27:36 -0600

    adding reporting to migration

--- migrations/addDatesToPhotos.js
+++ migrations/addDatesToPhotos.js
@@ -7,6 +7,7 @@ const tick = () => {
   mongoose.model('Photo').find({ __date: { $exists: false } }).limit(300)
     .then( photos => Promise.map(photos, photo => photo.save() ) )
     .then( photos => {
+      console.log(new Date(), '>', photos.length, 'saved');
       if(photos.length) { tick(); }
     });
 };
