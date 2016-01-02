cdc5b800f55f184bed552aba7c7bb40c4741dadb
Zeke Nierenberg <hampzan09@gmail.com>
2016-01-02 12:34:11 -0600

    making it walk

--- migrations/addDatesToPhotos.js
+++ migrations/addDatesToPhotos.js
@@ -3,12 +3,12 @@
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
 
-const tick = () => {
-  mongoose.model('Photo').find({ /*__date: { $exists: false }*/ }).limit(300)
+const tick = (i) => {
+  mongoose.model('Photo').find({ /*__date: { $exists: false }*/ }).limit(300).skip(i)
     .then( photos => Promise.map(photos, photo => photo.save() ) )
     .then( photos => {
       console.log(new Date(), '>', photos.length, 'saved');
-      if(photos.length) { tick(); }
+      if(photos.length) { tick(i+300); }
     });
 };
 
