174490e57bede057dcf0b828c18272bd6e1bc1e7
Zeke Nierenberg <hampzan09@gmail.com>
2016-01-01 19:05:11 -0600

    plugging in after

--- models/temperatures.js
+++ models/temperatures.js
@@ -7,5 +7,5 @@ const TemperatureSchema = new mongoose.Schema({
   value: { type: Number, required: true }
 });
 
-timeseriesPlugin.add(TemperatureSchema, 'Temperature');
 module.exports = mongoose.model('Temperature', TemperatureSchema);
+timeseriesPlugin.add(TemperatureSchema, 'Temperature');
