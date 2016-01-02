3f2b076c385c3e215d31692c3b33529a12fe4803
Zeke Nierenberg <hampzan09@gmail.com>
2016-01-02 12:32:19 -0600

    removing migration restriction

--- migrations/addDatesToPhotos.js
+++ migrations/addDatesToPhotos.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Promise = require('bluebird');
 
 const tick = () => {
-  mongoose.model('Photo').find({ __date: { $exists: false } }).limit(300)
+  mongoose.model('Photo').find({ /*__date: { $exists: false }*/ }).limit(300)
     .then( photos => Promise.map(photos, photo => photo.save() ) )
     .then( photos => {
       console.log(new Date(), '>', photos.length, 'saved');
