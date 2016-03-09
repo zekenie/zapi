f112a851ea4140e12da2de118bd1a47bc9c5a23f
Zeke Nierenberg <hampzan09@gmail.com>
2016-03-09 15:09:36 -0600

    oops

--- models/macstats.js
+++ models/macstats.js
@@ -9,7 +9,7 @@ const cap = string => string.charAt(0).toUpperCase() + string.slice(1);
 'macbook_battery_temp',
 'macbook_cpu_temp',
 'macbook_ram',
-'macbook_cpu_usage'].forEach(function() {
+'macbook_cpu_usage'].forEach(function(name) {
   const schema = new mongoose.Schema({
     value: { type: Number, required: true }
   });
