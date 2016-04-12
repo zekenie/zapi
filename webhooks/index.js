d27277b53d2c2411b9025993e3e11a3afc24fce2
Zeke Nierenberg <hampzan09@gmail.com>
2016-04-12 13:16:28 -0500

    oops

--- webhooks/index.js
+++ webhooks/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const router = module.exports = require('express').Router;
+const express = require('express');
+const router = express.Router();
 const bodyParser = require('body-parser');
-const model = require('../models').money.PlaidWebhook;
 
-router.use(bodyParser.json());
+router.use(bodyParser.json({ limit: '300kb' }));
 router.post('/plaid', (req, res, next) => {
   model.create(req.body)
     .then( () => res.status(201).end() )
     .catch(next);
 });
+
+module.exports = router;
