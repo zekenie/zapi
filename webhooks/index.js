1f015d1dea6cbe8bd0bd0b4394b74a8380c89222
Zeke Nierenberg <hampzan09@gmail.com>
2016-04-12 13:21:20 -0500

    trying to fix reference error

--- webhooks/index.js
+++ webhooks/index.js
@@ -3,11 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const model = require('../models').money.PlaidWebhook;
 
 router.use(bodyParser.json({ limit: '300kb' }));
 router.post('/plaid', (req, res, next) => {
-  model.create(req.body)
+  mongoose.model('PlaidWebhook').create(req.body)
     .then( () => res.status(201).end() )
     .catch(next);
 });
