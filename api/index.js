aa2df34cf70dd0b9a4bb9cb22e08a943077bee80
Zeke Nierenberg <hampzan09@gmail.com>
2016-05-02 19:16:02 -0500

    trying to get the server back up

--- api/index.js
+++ api/index.js
@@ -9,28 +9,29 @@ const fs = require('fs');
 module.exports = router;
 
 router.use((req, res, next) => {
-  if(Number(req.headers['Content-Length']) > (8800 * 1024)) {
-    console.log('big request found!');
-    // return res.status(200).end();
-    rawBody(req)
-      .then(buffer => {
-        return new Promise((resolve, reject) => {
-          fs.writeFile(__dirname + './tempRequest', buffer, (err, result) => {
-            if(err) { return reject(err); }
-            resolve(result);
-          });
+  console.log('request body size', Number(req.headers['content-length']));
+  // if(Number(req.headers['Content-Length']) > (8800 * 1024)) {
+  //   console.log('big request found!');
+  //   // return res.status(200).end();
+  //   rawBody(req)
+  //     .then(buffer => {
+  //       return new Promise((resolve, reject) => {
+  //         fs.writeFile(__dirname + './tempRequest', buffer, (err, result) => {
+  //           if(err) { return reject(err); }
+  //           resolve(result);
+  //         });
           
-        });
-      })
-      .then(() => console.log('logged HUGE request'))
-      .then(() => res.send(200))
-      .catch(next);
-  }
+  //       });
+  //     })
+  //     .then(() => console.log('logged HUGE request'))
+  //     .then(() => res.send(200))
+  //     .catch(next);
+  // }
   next();
-})
+});
 
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json({ limit: '11mb' }));
+router.use(bodyParser.json({ limit: '1mb' }));
 
 router.use('/photos', require('./photos'));
 router.use('/screenshots', require('./screenshots'));
